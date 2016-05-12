b04ead5dbbda702bfbf571d44b5240a75b2207a4
nnam-ibe <nnam.ibe@gmail.com>
2016-05-11 20:52:01 -0400

    Re-committing project

---
+++ app/src/main/java/stabs/com/pro_fi/MainActivity.java
@@ -0,0 +1,13 @@
+package stabs.com.pro_fi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
