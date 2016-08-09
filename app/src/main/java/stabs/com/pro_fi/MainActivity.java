45ae8cf5731db756af34c506460e9bdd5ce16bb4
OGBA <ogbarules@rocketmail.com>
2016-08-09 19:00:39 -0400

    First Activity

--- app/src/main/java/stabs/com/pro_fi/MainActivity.java
+++ app/src/main/java/stabs/com/pro_fi/MainActivity.java
@@ -1,42 +1,104 @@
 package stabs.com.pro_fi;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private Switch myswitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myswitch = (Switch) findViewById(R.id.mySwitch);
         ArrayList<String> list = new ArrayList<String>();
         list.add("Home");
         list.add("Work");
-
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        list.add("School");
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         if (recyclerView != null) {
             recyclerView.setHasFixedSize(true);
-            RecyclerView.LayoutManager layoutManager =  new LinearLayoutManager(this);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
             recyclerView.setLayoutManager(layoutManager);
             RecyclerView.Adapter mAdapter = new ProfileAdapter(list);
             recyclerView.setAdapter(mAdapter);
         }
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
     }
 
-    public void refreshConnection(View view){
+    public void refreshConnection(View view) {
         //Something
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://stabs.com.pro_fi/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://stabs.com.pro_fi/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- app/src/main/java/stabs/com/pro_fi/ProfileAdapter.java
+++ app/src/main/java/stabs/com/pro_fi/ProfileAdapter.java
@@ -1,10 +1,19 @@
 package stabs.com.pro_fi;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,13 +21,30 @@ import java.util.ArrayList;
  * Adapter to manage the recycler view.
  */
 public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
-
+    ProfileAdapter context=this;
     public ArrayList<String> profileNames;
 
-    public ProfileAdapter(ArrayList<String> list) {
+    public ProfileAdapter(ArrayList<String> list)
+    {
         profileNames = list;
     }
-
+    public static void delete_Diag(View view) {
+        new AlertDialog.Builder(view.getContext())
+                .setTitle("Delete Profile")
+                .setMessage("Are you sure you want to delete this Profile?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
     @Override
     public ProfileAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -43,6 +69,33 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHold
         public ViewHolder(View v) {
             super(v);
             textView = (TextView)v.findViewById(R.id.name_text_view);
+            final Button button1 = (Button)v.findViewById(R.id.button);
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View v) {
+                    //Creating the instance of PopupMenu
+                    PopupMenu popup = new PopupMenu(v.getContext(), button1);
+                    //Inflating the Popup using xml file
+                    popup.getMenuInflater()
+                            .inflate(R.menu.popup_menu, popup.getMenu());
+
+                    //registering popup with OnMenuItemClickListener
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            if(item.getTitle().equals("Delete")){delete_Diag(v);}
+                            Toast.makeText(
+                                    v.getContext(),
+                                    "You Clicked : " + item.getTitle(),
+                                    Toast.LENGTH_SHORT
+                            ).show();
+                            return true;
+                        }
+                    });
+                    popup.setGravity(Gravity.RIGHT);
+
+                    popup.show(); //showing popup menu
+                }
+                });
         }
     }
 
