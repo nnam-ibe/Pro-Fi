35965d282d4b9c75d3a424ec5a6891fb6b2e5754
nnam-ibe <nnam.ibe@gmail.com>
2017-08-19 09:46:59 -0400

    Updated icon

--- app/src/main/java/stabs/com/pro_fi/MainActivity.java
+++ app/src/main/java/stabs/com/pro_fi/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
